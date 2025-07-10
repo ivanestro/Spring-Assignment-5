a7a72de3c3e2169d1a3f595f7eb4a6a4319c99a7
ivanestro <iestropigan@rrc.ca>
2025-07-09 22:41:50 -0500

    Initial commit: Set up project directory and file structure.

---
+++ tests/test_chatbot.py
@@ -0,0 +1,19 @@
+"""This module defines the TestChatbot class.
+
+The TestChatbot class contains unit test methods to test the 
+src.chatbot.Chatbot class.
+
+You must execute this module in command-line where your present
+working directory is the root directory of the project.
+
+Example:
+    python -m unittest tests/test_chatbot.py
+"""
+
+from unittest import TestCase, main
+from unittest.mock import patch
+from src.chatbot import ACCOUNTS, VALID_TASKS
+
+__author__ = "Ivan Estropigan"
+__version__ = "1.0"
+__credits__ = "COMP-1327 Faculty, W3School, FreeCodeCamp"
