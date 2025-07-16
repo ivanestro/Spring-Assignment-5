1433bbf0ef6801f6c2a841517eddca75b747172b
ivanestro <iestropigan@rrc.ca>
2025-07-16 15:51:51 -0500

    Update: Working with Account Number Input Validation.

--- src/chatbot.py
+++ src/chatbot.py
@@ -7,9 +7,11 @@ Example:
     $ python src/chatbot.py
 """
 
-__author__ = ""
-__version__ = ""
-__credits__ = "COMP-1327 Faculty"
+__author__ = "Ivan Estropigan"
+__version__ = "1.0"
+__credits__ = "COMP-1327 Faculty, W3School,freeCodeCamp, pylint.readthedocs.io"
+
+online = True
 
 ACCOUNTS = {
     123456: {
@@ -26,8 +28,12 @@ VALID_TASKS = [
     "exit"
 ]
 
+
 def chatbot():
-    """Performs the Chatbot functionality."""
+    """
+    Description:
+        Performs the Chatbot functionality."""
+    
     COMPANY_NAME = "PiXELL River Financial"
 
     # Print welcome message
@@ -37,5 +43,45 @@ def chatbot():
     # Print thank you message
     print(f"Thank you for banking with {COMPANY_NAME}.")
 
+def get_account_number() -> int:
+    """
+    Description:
+        User interaction verifies an account number in a loop
+
+    This validates the user that they enter an account number correctly
+    if not check in ACCOUNTS if it exists in the dictionary.
+
+    Args:
+        None
+
+    Returns:
+        int: To Validate account number
+
+    Raises:
+        TypeError: This raises an exception if the user input is a string
+        ValueError: This raises an exception if the user input does not exist
+        in the dictionary.
+    """
+
+    while online:
+        user_number = input("Please enter your account number: ")
+
+        try:
+            account_number = int(user_number)
+        
+        except ValueError as e:
+            raise TypeError("Account number must be an int type.") from e
+        
+        if account_number not in ACCOUNTS:
+            raise ValueError("Account number entered does not exist.")
+        
+        # If user did input correct account information
+        # return the account number.
+        if account_number in ACCOUNTS:
+            return account_number
+        
+        
 if __name__ == "__main__":
     chatbot()
+
+
