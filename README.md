08485a4510199babe2934d91019a5681726647e5
ivanestro <iestropigan@rrc.ca>
2025-07-16 18:10:37 -0500

    Update: Working with Unit Testing exceptions, and valid accounts

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Assignment 5
 
+In this assignment, you will have the opportunity to practice your knowledge of Python functions by creating a chatbot that assists customers with banking tasks. Prior to this assignment, you will have learned about git, variables and data types, exception handling, looping and data structures. By completing this assignment, you will learn valuable skills that are essential for a professional software developer such as code organization, modularity, and best practices for developing functions and corresponding unit tests.
+
+## Author
+
+Ivan Estropigan
+
 ## Requirements
 
 **UserStory**
@@ -9,9 +15,32 @@ I want to check my account balance and make deposits,
 
 So that I can keep track of my finances and make transactions easily.
 
+## Reflection
+
+### 1. Identify any challenges or issues you encountered while writing your functions
+
+- The ValueError and raise TypeError figuring out how I can implement this code.
+- For unit test I have issues trying to run the code as to what I should put if I am dealing with exception messages the self.assertEquals(str()) function. I had to re-look into the powerpoint/learning_module_5_part_2.
+-
+
+### 2. Discuss the benefits and challenges of developing and using unit tests
+
+- The benefit of developing unit test is to make sure that we capture bugs early on and time management to help lessen the time to figure out what went wrong rather than using the old one by one debugging.
+-
+-
+
 ## Code Modification 1
 
 - Added: Author name, Version, Credits
 - Added: user_number for users to interact what their account is.
 - Added: Defining get_account to validate if the user interacts with the correct dictionary.
 - Added: Raise TypeError and ValueError if user inputs wrong account either string/int not in dictionary.
+
+## Code Modification 2
+
+- Modified: Author, Version, Credits
+
+- Added: get_account_number to the from src.chatbot import ACCOUNTS, VALID_TASKS, get_account_number
+- Added: class TestChatBot(TestCase):
+- Added: defined each specific test_get_account to their desired functions of what they do. Such as:
+TypeError, ValueError, Matching Valid Account.
--- tests/test_chatbot.py
+++ tests/test_chatbot.py
@@ -12,8 +12,67 @@ Example:
 
 from unittest import TestCase, main
 from unittest.mock import patch
-from src.chatbot import ACCOUNTS, VALID_TASKS
+from src.chatbot import ACCOUNTS, VALID_TASKS, get_account_number
 
 __author__ = "Ivan Estropigan"
 __version__ = "1.0"
-__credits__ = "COMP-1327 Faculty, W3School, FreeCodeCamp"
+__credits__ = "COMP-1327 Faculty, W3School, FreeCodeCamp, pylint.readthedocs.io"
+
+class TestChatBot(TestCase):
+    
+    
+    def test_get_account_number_raises_type_error(self):
+        # Arrange
+        user_input = "non_numeric_data" # Checking when user_input as test
+
+        # Act
+        # Mock the input function to return "non_numeric_data"
+        # To simulate user_input
+        with patch('builtins.input', return_value = user_input):
+            with self.assertRaises(TypeError)as e:
+                get_account_number()
+        
+        # Assert
+        # Exception Message
+        # str is to handle string messages
+        # exception for errors
+        expected_message = "Account number must be an int type."
+        self.assertEqual(str(e.exception), expected_message)
+
+    def test_get_account_number_raises_value_error(self):
+        # Arrange
+        user_input = "112233"
+
+        # Act
+        # mock the input function to return "112233"
+        # Simulating user_input
+        with patch('builtins.input', return_value = user_input):
+            with self.assertRaises(ValueError)as e:
+                get_account_number()
+        
+        # Assert
+        # Exception Message as expected_message
+        # str is to handle string messages
+        # exception for errors
+        expected_message = "Account number entered does not exist."
+        self.assertEqual(str(e.exception), expected_message)
+
+    
+    def test_get_account_number_verify_valid_account(self):
+        # Arrange
+        user_input = "123456"
+        expected_account = 123456
+
+        # Act
+        # mock the user_input to return 123456
+        # return the user_input 123456
+        with patch('builtins.input', return_value = user_input):
+                account_123456 = get_account_number()
+
+        # Assert
+        # does the account number equals?
+        # if so ok if not error
+        self.assertEqual(account_123456, expected_account)
+
+if __name__ == "main":
+    main()
